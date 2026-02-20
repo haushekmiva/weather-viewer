0c965c908b206e0a9bf093b8a4422f31f1e04b67
haushekmiva <chuminitoGordito@proton.me>
2026-02-20 19:47:35 +0300

    chore: add application config

---
+++ src/main/java/com/haushekmiva/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.haushekmiva.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.haushekmiva")
+public class AppConfig {
+
+}
