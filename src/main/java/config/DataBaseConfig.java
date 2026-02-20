adfd0c7cdbf002ba3c4769ebb95f4af773b4151e
haushekmiva <chuminitoGordito@proton.me>
2026-02-20 19:30:33 +0300

    chore: configure database

---
+++ src/main/java/config/DataBaseConfig.java
@@ -0,0 +1,91 @@
+package config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class DataBaseConfig {
+
+    // чтение application.properties и запись значений в переменные
+    @Value("${db.url}")
+    private String dbUrl;
+
+    @Value("${db.username}")
+    private String dbUsername;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
+    @Value("${db.pool.size}")
+    private int dbPoolSize;
+
+    @Value("${hibernate.show_sql}")
+    private String showSql;
+
+    @Value("${hibernate.format_sql}")
+    private String formatSql;
+
+    @Value("${hibernate.dialect}")
+    private String dialect;
+
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String ddlAuto;
+
+    // настраиваем пулл соединений
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(dbUsername);
+        config.setPassword(dbPassword);
+        config.setMaximumPoolSize(dbPoolSize);
+        return new HikariDataSource(config);
+    }
+
+    // настраиваем миграции
+    @Bean
+    public Flyway flyway(DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+        flyway.migrate();
+        return flyway;
+    }
+
+    // создаем и настраиваем фабрику сессий
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource, Flyway flyway) {
+        LocalSessionFactoryBean sessionFactory  = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan("com.haushekmiva.model");
+
+        Properties props = new Properties();
+
+        props.setProperty("hibernate.hbm2ddl.auto", ddlAuto);
+        props.setProperty("hibernate.dialect", dialect);
+        props.setProperty("hibernate.show_sql", showSql);
+        props.setProperty("hibernate.format_sql", formatSql);
+
+        sessionFactory.setHibernateProperties(props);
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
+    }
+}
