b2781721cae28d4daf85a0a5e92ae6b303a49edd
haushekmiva <chuminitoGordito@proton.me>
2026-02-24 22:30:39 +0300

    refactor: remove custom repository exceptions to use standard Spring ones

--- src/main/java/com/haushekmiva/repository/UserRepositoryImpl.java
+++ src/main/java/com/haushekmiva/repository/UserRepositoryImpl.java
@@ -1,11 +1,7 @@
 package com.haushekmiva.repository;
 
-import com.haushekmiva.exception.custom.DataBaseException;
-import com.haushekmiva.exception.custom.DuplicateEntryException;
 import com.haushekmiva.model.User;
-import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,41 +19,26 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public void createUser(User user) {
-        try {
             sessionFactory.getCurrentSession().persist(user);
-        } catch (ConstraintViolationException e) {
-            throw new DuplicateEntryException("Constraint violation on insert: duplicate login '%s'"
-                    .formatted(user.getLogin()), e);
-        }
     }
 
     @Transactional(readOnly = true)
     @Override
     public Optional<User> getUserByLogin(String login) {
-        try {
-            return sessionFactory.getCurrentSession()
-                    .createQuery("FROM User u WHERE u.login = :login", User.class)
-                    .setParameter("login", login)
-                    .uniqueResultOptional();
-        } catch (HibernateException e) {
-            throw new DataBaseException("An error occurred while getting user '%s'.".formatted(login), e);
-        }
+        return sessionFactory.getCurrentSession()
+                .createQuery("FROM User u WHERE u.login = :login", User.class)
+                .setParameter("login", login)
+                .uniqueResultOptional();
     }
 
     @Transactional(readOnly = true)
     @Override
     public boolean isUserExists(String login) {
-        try {
-            Long userCount = (sessionFactory.getCurrentSession()
-                    .createQuery("SELECT COUNT(u) FROM User u WHERE u.login = :login", Long.class)
-                    .setParameter("login", login)
-                    .uniqueResult()
-            );
-
-            return userCount > 0;
-        } catch (HibernateException e) {
-            throw new DataBaseException("An error occurred while checking if the user '%s' exists.".formatted(login),
-                    e);
-        }
+        Long userCount = (sessionFactory.getCurrentSession()
+                .createQuery("SELECT COUNT(u) FROM User u WHERE u.login = :login", Long.class)
+                .setParameter("login", login)
+                .uniqueResult()
+        );
+        return userCount > 0;
     }
 }
