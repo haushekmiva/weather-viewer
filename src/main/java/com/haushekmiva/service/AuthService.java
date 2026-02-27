a185391a0bdb5f4ffff370c436e637568fc9dbb0
haushekmiva <chuminitoGordito@proton.me>
2026-02-27 22:57:39 +0300

    refactor: update auth service interface

--- src/main/java/com/haushekmiva/service/AuthService.java
+++ src/main/java/com/haushekmiva/service/AuthService.java
@@ -1,12 +1,14 @@
 package com.haushekmiva.service;
 
 import com.haushekmiva.dto.AuthResponse;
+import com.haushekmiva.dto.UserLoginRequest;
+import com.haushekmiva.dto.UserRegisterRequest;
 
 import java.util.UUID;
 
 public interface AuthService {
     AuthResponse registerUser(UserRegisterRequest userRegisterRequest);
     AuthResponse loginUser(UserLoginRequest userAuthResponse);
-    boolean logoutUser(UserLogoutRequest userLogoutRequest);
+    boolean logoutUser(UUID sessionId);
     boolean isUserAuthenticated(UUID sessionId);
 }
