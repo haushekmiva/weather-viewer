1d052662d26b1c1d90c4b364703d08fe58afaab9
haushekmiva <chuminitoGordito@proton.me>
2026-02-28 19:02:13 +0300

    refactor: change returning value in isUserAuthenticated

--- src/main/java/com/haushekmiva/service/AuthService.java
+++ src/main/java/com/haushekmiva/service/AuthService.java
@@ -4,11 +4,12 @@ import com.haushekmiva.dto.AuthResponse;
 import com.haushekmiva.dto.UserLoginRequest;
 import com.haushekmiva.dto.UserRegisterRequest;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AuthService {
     AuthResponse registerUser(UserRegisterRequest userRegisterRequest);
     AuthResponse loginUser(UserLoginRequest userAuthResponse);
     boolean logoutUser(UUID sessionId);
-    boolean isUserAuthenticated(UUID sessionId);
+    Optional<UserDto> isUserAuthenticated(UUID sessionId);
 }
