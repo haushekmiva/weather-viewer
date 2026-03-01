c4bfa180915bc74bfe30b9c63f9370da50ab95ca
haushekmiva <chuminitoGordito@proton.me>
2026-03-01 21:20:14 +0300

    feat: add register implementation in AuthService

---
+++ src/main/java/com/haushekmiva/service/AuthServiceImpl.java
@@ -0,0 +1,67 @@
+package com.haushekmiva.service;
+
+import com.haushekmiva.dto.*;
+import com.haushekmiva.mapper.UserMapper;
+import com.haushekmiva.model.Session;
+import com.haushekmiva.model.User;
+import com.haushekmiva.repository.SessionRepository;
+import com.haushekmiva.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    public static final int DAYS_IN_WEEK = 7;
+
+    private final UserRepository userRepository;
+    private final SessionRepository sessionRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional
+    public AuthResponse registerUser(UserRegisterRequest userRegisterRequest) {
+        if (!Objects.equals(userRegisterRequest.password(), userRegisterRequest.repeatPassword())) {
+            return new AuthError("Passwords do not match.");
+        }
+
+        String passwordHash = passwordEncoder.encode(userRegisterRequest.password());
+
+        User user = new User(userRegisterRequest.username(), passwordHash);
+        try {
+            userRepository.create(user);
+        } catch (DataIntegrityViolationException e) {
+            return new AuthError("Invalid username or password.");
+        }
+
+        UUID sessionId = UUID.randomUUID();
+        sessionRepository.create(new Session(sessionId, LocalDateTime.now().plusDays(DAYS_IN_WEEK), user));
+
+        return new AuthSuccess(sessionId);
+    }
+
+    @Override
+    public AuthResponse loginUser(UserLoginRequest userLoginRequest) {
+        return null;
+    }
+
+    @Override
+    public boolean logoutUser(UUID sessionId) {
+        return false;
+    }
+
+    @Override
+    public Optional<UserDto> isUserAuthenticated(UUID sessionId) {
+        return Optional.empty();
+    }
+}
