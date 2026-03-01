e0d2e52e446d58c5144d78c2bc20f6825c890fc6
haushekmiva <chuminitoGordito@proton.me>
2026-03-01 21:17:36 +0300

    chore: add Bcrypt bean

--- src/main/java/com/haushekmiva/config/AppConfig.java
+++ src/main/java/com/haushekmiva/config/AppConfig.java
@@ -1,12 +1,20 @@
 package com.haushekmiva.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ComponentScan("com.haushekmiva")
 @PropertySource("classpath:application.properties")
 public class AppConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 
 }
