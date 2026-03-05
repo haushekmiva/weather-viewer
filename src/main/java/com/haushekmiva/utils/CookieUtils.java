691b3ad953099be97a781162fad12a4f1a3c1cfc
haushekmiva <chuminitoGordito@proton.me>
2026-03-05 20:02:10 +0300

    feat: add CookieUtils

---
+++ src/main/java/com/haushekmiva/utils/CookieUtils.java
@@ -0,0 +1,16 @@
+package com.haushekmiva.utils;
+
+import jakarta.servlet.http.Cookie;
+
+public final class CookieUtils {
+
+    private CookieUtils() {}
+
+    public static Cookie cookie(String name, String value, int maxAge, String path) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setMaxAge(maxAge);
+        cookie.setPath(path);
+        cookie.setHttpOnly(true);
+        return cookie;
+    }
+}
