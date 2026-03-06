3c5154aa69ca0c91db1246e204487cc6524753af
haushekmiva <chuminitoGordito@proton.me>
2026-03-06 18:12:45 +0300

    fix: update UserService interface method signature
    Missed commit, should have been added before implementation

--- src/main/java/com/haushekmiva/service/AuthService.java
+++ src/main/java/com/haushekmiva/service/AuthService.java
@@ -1,9 +1,6 @@
 package com.haushekmiva.service;
 
-import com.haushekmiva.dto.AuthResponse;
-import com.haushekmiva.dto.UserDto;
-import com.haushekmiva.dto.UserLoginRequest;
-import com.haushekmiva.dto.UserRegisterRequest;
+import com.haushekmiva.dto.*;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -12,5 +9,6 @@ public interface AuthService {
     AuthResponse registerUser(UserRegisterRequest userRegisterRequest);
     AuthResponse loginUser(UserLoginRequest userLoginRequest);
     void logoutUser(UUID sessionId);
-    Optional<UserDto> getUserBySessionId(UUID sessionId);
+    Optional<UserDto> getUser(UUID sessionId);
+    Optional<UserDtoWithLocations> getUserWithLocations(UUID sessionId);
 }
