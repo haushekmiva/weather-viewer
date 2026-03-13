7ee0e283fcc1ba87b173516f6b6065064775252a
haushekmiva <chuminitoGordito@proton.me>
2026-03-13 19:53:11 +0300

    test: add registerUser_correctInput_newSessionCreated

--- src/test/java/com/haushekmiva/service/AuthServiceTest.java
+++ src/test/java/com/haushekmiva/service/AuthServiceTest.java
@@ -1,8 +1,12 @@
 package com.haushekmiva.service;
 
 import com.haushekmiva.BaseIntegrationTest;
+import com.haushekmiva.dto.AuthResponse;
+import com.haushekmiva.dto.AuthSuccess;
 import com.haushekmiva.dto.UserRegisterRequest;
+import com.haushekmiva.model.Session;
 import com.haushekmiva.model.User;
+import com.haushekmiva.repository.SessionRepository;
 import com.haushekmiva.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
@@ -23,6 +27,7 @@ public class AuthServiceTest extends BaseIntegrationTest {
 
     private final AuthService authService;
     private final UserRepository userRepository;
+    private final SessionRepository sessionRepository;
 
 
     @Test
@@ -37,5 +42,22 @@ public class AuthServiceTest extends BaseIntegrationTest {
         assertThat(user.get().getLogin())
                 .withFailMessage("Entry should have username equal to %s", TEST_USERNAME)
                 .isEqualTo(TEST_USERNAME);
+    }
+
+    @Test
+    public void registerUser_correctInput_newSessionCreated() {
+        AuthResponse authResponse = authService.registerUser(
+                new UserRegisterRequest(TEST_USERNAME, TEST_PASSWORD, TEST_PASSWORD)
+        );
+
+        assertThat(authResponse).isInstanceOf(AuthSuccess.class);
+
+        AuthSuccess authSuccess = (AuthSuccess) authResponse;
+
+        Optional<Session> session = sessionRepository.getById(authSuccess.sessionId());
+
+        assertThat(session)
+                .withFailMessage("Session for user with username %s no exists.", TEST_USERNAME)
+                .isPresent();
     }
 }
