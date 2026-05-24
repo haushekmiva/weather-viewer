a82ad3f7c21c1e8887a059c5b640fe9f42f97dc9
haushekmiva <chuminitoGordito@proton.me>
2026-05-24 19:22:37 +0300

    refactor: change arguments of super in constructor

--- src/main/java/com/haushekmiva/exception/custom/ExternalApiException.java
+++ src/main/java/com/haushekmiva/exception/custom/ExternalApiException.java
@@ -4,12 +4,10 @@ import lombok.Getter;
 
 @Getter
 public class ExternalApiException extends InfrastructureException {
-    private final String serviceName;
     private final int httpStatus;
 
-    public ExternalApiException(String message, Throwable e, String serviceName, int errorCode) {
-        super(message, e);
-        this.serviceName = serviceName;
+    public ExternalApiException(String message, int errorCode) {
+        super(message);
         this.httpStatus = errorCode;
     }
 }
