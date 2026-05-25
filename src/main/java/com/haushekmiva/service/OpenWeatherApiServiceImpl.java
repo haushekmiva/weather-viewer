87a3a864e3f4d6b8ff94f33a1044f442f39ce51e
haushekmiva <chuminitoGordito@proton.me>
2026-05-25 14:52:35 +0300

    fix: add missing @Service annotation to OpenWeatherApiServiceImpl

--- src/main/java/com/haushekmiva/service/OpenWeatherApiServiceImpl.java
+++ src/main/java/com/haushekmiva/service/OpenWeatherApiServiceImpl.java
@@ -10,12 +10,14 @@ import com.haushekmiva.utils.ValidUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@Service
 @RequiredArgsConstructor
 public class OpenWeatherApiServiceImpl implements OpenWeatherApiService {
 
