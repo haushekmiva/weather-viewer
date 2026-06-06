9a0b60d90a4bf950598a84ec5250736eff2bdbf3
haushekmiva <chuminitoGordito@proton.me>
2026-06-06 15:27:24 +0300

    refactor: add description field in OpenWeatherDto

--- src/main/java/com/haushekmiva/dto/OpenWeatherDto.java
+++ src/main/java/com/haushekmiva/dto/OpenWeatherDto.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OpenWeatherDto {
@@ -15,6 +17,15 @@ public class OpenWeatherDto {
 
     @JsonProperty("sys")
     private SysData sysInfo;
+
+    @JsonProperty("weather")
+    private List<WeatherElement> weather;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class WeatherElement {
+        private String description;
+    }
 
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
