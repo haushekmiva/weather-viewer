a3b322dcda34b5d9d7e301e1f9f8c024195f40e5
haushekmiva <chuminitoGordito@proton.me>
2026-06-06 15:35:22 +0300

    refactor: add new description and icon field processing in WeatherMapper

--- src/main/java/com/haushekmiva/mapper/WeatherMapper.java
+++ src/main/java/com/haushekmiva/mapper/WeatherMapper.java
@@ -4,6 +4,9 @@ import com.haushekmiva.dto.OpenWeatherDto;
 import com.haushekmiva.dto.WeatherDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 
@@ -13,5 +16,27 @@ public interface WeatherMapper {
     @Mapping(source = "mainInfo.temp", target = "temp")
     @Mapping(source = "mainInfo.feelsLike", target = "feelsLike")
     @Mapping(source = "mainInfo.humidity", target = "humidity")
+    @Mapping(source = "weather", target = "description", qualifiedByName = "firstWeatherDescription")
+    @Mapping(source = "weather", target = "icon", qualifiedByName = "firstWeatherIcon")
     WeatherDto toDto(OpenWeatherDto openWeatherDto);
+
+    @Named("firstWeatherDescription")
+    default String extractFirstWeather(List<OpenWeatherDto.WeatherElement> weathers) {
+        if (weathers == null || weathers.isEmpty()) {
+            return null;
+        }
+
+        return weathers.getFirst().getDescription();
+    }
+
+    @Named("firstWeatherIcon")
+    default String extractFirstIcon(List<OpenWeatherDto.WeatherElement> weathers) {
+        if (weathers == null || weathers.isEmpty()) {
+            return null;
+        }
+
+        return weathers.getFirst().getIcon();
+    }
+
+
 }
