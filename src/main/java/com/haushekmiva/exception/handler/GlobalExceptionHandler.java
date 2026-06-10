4c9cab2b5dfc9eb1cfe4a4755bc101fec9e7bf78
haushekmiva <chuminitoGordito@proton.me>
2026-06-10 16:02:11 +0300

    feat: add GlobalExceptionHandler and error page template

--- src/main/java/com/haushekmiva/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/haushekmiva/exception/handler/GlobalExceptionHandler.java
@@ -1,10 +1,18 @@
 package com.haushekmiva.exception.handler;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;    
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-
-
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleGeneral(Model model) {
+        model.addAttribute("message", "Something went wrong.");
+        return "error";
+    }
 }
