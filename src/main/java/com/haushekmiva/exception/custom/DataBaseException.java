46d4c0a5de32a2e64f259ee97d2d077cd769d32a
haushekmiva <chuminitoGordito@proton.me>
2026-06-11 18:52:07 +0300

    refactor: remove unused custom exceptions

--- src/main/java/com/haushekmiva/exception/custom/DataBaseException.java
+++
@@ -1,7 +0,0 @@
-package com.haushekmiva.exception.custom;
-
-public class DataBaseException extends InfrastructureException {
-    public DataBaseException(String message, Throwable e) {
-        super(message, e);
-    }
-}
--- src/main/java/com/haushekmiva/exception/custom/DomainLogicException.java
+++
@@ -1,7 +0,0 @@
-package com.haushekmiva.exception.custom;
-
-public class DomainLogicException extends AppException {
-    public DomainLogicException(String message, Throwable e) {
-        super(message, e);
-    }
-}
