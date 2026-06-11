7fbf257e5834e5997caadf4731b425b81ff9e8fc
haushekmiva <chuminitoGordito@proton.me>
2026-06-11 16:29:50 +0300

    feat: add exception logging and logback configuration

--- src/main/java/com/haushekmiva/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/haushekmiva/exception/handler/GlobalExceptionHandler.java
@@ -1,18 +1,22 @@
 package com.haushekmiva.exception.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleGeneral(Model model) {
+    public String handleGeneral(Exception e, Model model) {
         model.addAttribute("message", "Something went wrong.");
+
+        log.error("Error occurred.", e);
         return "error";
     }
 }
